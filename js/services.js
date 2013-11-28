bd549c90a48922001bc02eb5cf27fdc764674dea
Andrew Ellerton <andrew@ellerton.net>
2013-11-28 14:27:49 +0800

    Overhauled original.
    Steps forward:
    - corrected code style to 2 spaces, no tabs, { on end of line.
    - removed all local storage work, except left notes in services.js to
      show how it could be done in a clean way.
    - cleanly separated list.html vs add.html
    - removed all jquery and using angular-ui for all Bootstrap JS.
    - modal and accordion fully working with angular-ui, no jquery
    - split controllers to be just controllers, and added a RecipeService that
      does the work of owning, adding and updating recipes.
    - removed real functionality from "Fav". I didn't really understand what
      was going on there. See templates/fav.html.
    - sorted list of recipes alphabetically.
    
    Steps back:
    - did not re-introduce local storage, but left notes in services.js to show
      how it could be added cleanly using events.
    - the use of angular-ui meant I downgraded bootstrap to 2.3.2. Unfortunately
      angular-ui doesn't fully support bootstrap 3 yet.
    - I didn't update any of the styles to work again, so it looks very plain.

--- js/services.js
+++ js/services.js
@@ -1,7 +1,76 @@
-angular.module('Services', ['ngResource']).
-factory('Recipes', function($resource){
-    return $resource('recipes.json', {}, {
-        query: {method:'GET', params:{File:'Json'}, isArray:false},
-        update: {method:'PUT', params:{File:'Json'}}
-    });
-});
+
+angular.module('RecipeServiceFactory', [])
+
+  .factory ('RecipeService', function () {
+
+    // NOTE: See notes about RecipeStorageService below.
+
+    var data = [ {
+      name: 'Ladoo',
+      preparation: 'Create paste using besan,salt and color mix. Heat oil to medium temperature. Add this mixture into filter and stir nicely to create small balls. Now add this into a sugar syrup. AFter 30 mins create balls using of any size. Ladoo ready'
+    }, {
+      name: 'PepperChicken',
+      preparation: 'Pepper chicken is one the spicy receipe. Pepper chicken is one the spicy receipe. Pepper chicken is one the spicy receipe. Pepper chicken is one the spicy receipe. Pepper chicken is one the spicy receipe.'
+    }, {
+      name: 'MuttonCurry',
+      preparation: 'Mutton curry is one of mmy fav receipe. Mutton curry is one of mmy fav receipe. Mutton curry is one of mmy fav receipe. Mutton curry is one of mmy fav receipe. Mutton curry is one of mmy fav receipe. '
+    }];
+
+    return {
+      recipes: function () {
+        return data;
+      },
+      get: function(id){
+        return data[id];
+      },
+      add: function (recipe) {
+        data.push({
+          name : recipe.name, preparation : recipe.preparation
+        });
+        // NOTE: At this point could broadcast an event that data has changed,
+        // would a listener could then back to local storage.
+        return data;
+      },
+      update: function (recipe, index) {
+        data[index] = recipe;
+        // NOTE: At this point could broadcast an event that data has changed,
+        // would a listener could then back to local storage.
+        return data;
+      },
+    }
+  }) // end of Recipes service
+
+  .factory ('RecipeStorageService', function () {
+
+    /* TODO: could manage all local storage in here.
+             The RecipeService would call this for the initial set of data,
+             which would get it from local storage (if it's there) or initialise it
+             (if it's not in storage).
+
+             Then when RecipeService changes data, it would broadcast an event
+             and this service would persist changes to local storage.
+
+       NOTES from Ramya's original
+
+       localStorage.setItem("recipes", JSON.stringify($scope.recipes));
+
+       $scope.saved = localStorage.getItem('recipes');
+       $scope.recipes = (localStorage.getItem('recipes')!==null) ? JSON.parse($scope.saved) : [ ];
+       localStorage.setItem('recipes', JSON.stringify($scope.recipes));
+       $scope.list = function(){
+         if(typeof $scope.recipes ==='undefined') {
+           if(localStorage.getItem("recipes") ===null) {
+             $scope.recipes = [];
+           } else {
+             $scope.recipes = JSON.parse(localStorage.getItem("recipes"));
+           }
+         } else {
+           console.log('num recipes'+$scope.recipes.length);
+         }
+       };
+
+    */
+
+  } // end of Recipes Storage service
+);
+
