337d3418819695664627c68c0bb4b32c3de1381f
ramsz082 <ramsz082@gmail.com>
2014-03-18 14:02:36 +0800

    Reverting to previous commit

--- js/controller.js
+++ js/controller.js
@@ -4,47 +4,89 @@ var cookApp = angular.module('cookApp', ['ngRoute', 'ui.bootstrap', 'RecipeServi
 cookApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/home', {
-  templateUrl: 'templates/home.html',
-  controller: 'homeCtrl'
+      when('/recipes', {
+	templateUrl: 'templates/list.html',
+	controller: 'RecipeListCtrl'
       }).
-      when('/tables', {
-  templateUrl: 'templates/tables.html',
-  controller: 'tableCtrl'
-      }).
-      when('/order', {
-  templateUrl: 'templates/order.html',
-  controller: 'menuCtrl'
-      }).
-      when('/neworder', {
-  templateUrl: 'templates/new-order.html',
-  controller: 'neworderCtrl'
-      }).
-      when('/changeorder', {
-  templateUrl: 'templates/change-order.html',
-  controller: 'changeorderCtrl'
-      }).
-      when('/closeorder', {
-  templateUrl: 'templates/close-order.html',
-  controller: 'closeOrderCtrl'
-      }).
-      when('/showreceipt', {
-  templateUrl: 'templates/show-receipt.html',
-  controller: 'showReceiptCtrl'
+      when('/fav', {
+	templateUrl: 'templates/fav.html',
+	controller: 'FavRecipeCtrl'
       }).
       otherwise({
-  redirectTo: '/home'
+	redirectTo: '/recipes'
       });
 }]);
 
+
+var RecipeModalInstanceCtrl = function ($scope, $modalInstance, title, recipe, isFavRecipe) {
+
+  $scope.title = title;
+  $scope.recipe = recipe;
+
+  $scope.ok = function () {
+    $modalInstance.close($scope.recipe);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+};
+
 cookApp.controller('RecipeListCtrl', function ($scope, $modal, $log, RecipeService) {
 
-  
+  $scope.recipes = RecipeService.recipes();
+  $scope.add = function() {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'templates/add.html',
+      controller: RecipeModalInstanceCtrl,
+      resolve: {
+        recipe: function () { return { name: "", preparation: "" }; },
+        title: function() { return "Add Recipe"; }
+      }
+    });
+
+    modalInstance.result.then(function (recipe) {
+      $scope.recipes = RecipeService.add(recipe);
+      
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+  $scope.edit = function(recipe) {
+    var index = $scope.recipes.indexOf(recipe);
+
+    var modalInstance = $modal.open({
+      templateUrl: 'templates/add.html',
+      controller: RecipeModalInstanceCtrl,
+      resolve: {
+        recipe: function () { return recipe; },
+        title: function() { return "Edit Recipe ("+index+": "+$scope.recipes[index].name+")"; }
+      }
+    });
+
+    modalInstance.result.then(function (recipe) {
+      $scope.recipes = RecipeService.update(index, recipe);
+      
+    }, function () {
+      $log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+	$scope.addFav = function(recipe) {
+	  $scope.recipes = RecipeService.addFav(recipe);	  
+	};
 });
 
 
-
-
-
+cookApp.controller('FavRecipeCtrl', function ($scope, RecipeStorageService, RecipeService) {
+	var favRecipes = RecipeService.recipes();
+	$scope.recipes = [];
+	angular.forEach(favRecipes, function(recipe) {
+		if (recipe.favitem == true) {
+		  $scope.recipes.push(recipe);
+		}
+	});	
+});
 
 
